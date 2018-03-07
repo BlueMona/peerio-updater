2c0a30a8078bfc43ede1f2ed429992a071b36ba0
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-03-07 13:48:52 +0100

    feat: failure handling

--- updater.js
+++ updater.js
@@ -1,6 +1,7 @@
 // @ts-check
 const semver = require('semver');
 const path = require('path');
+const fs = require('fs');
 const os = require('os');
 const crypto = require('crypto');
 const EventEmitter = require('events').EventEmitter;
@@ -17,6 +18,13 @@ const MANIFEST_FILENAME = 'manifest.txt';
 /** Check intervals */
 const DEFAULT_INTERVAL = 10 * 60 * 60 * 1000; // 10 hours
 const MIN_INTERVAL = 15 * 60 * 1000; // 15 minutes
+
+/**
+ * Update info file stores the information
+ * about update before attempting installation
+ * in the downloads directory.
+ */
+const UPDATE_INFO_FILENAME = 'update-info.json';
 
 class Updater extends EventEmitter {
     /**
@@ -87,32 +95,42 @@ class Updater extends EventEmitter {
      *
      * @param {number?} manifestURLIndex - optional manifest URL index to check
      */
-    checkForUpdates(manifestURLIndex = 0) {
+    async checkForUpdates(manifestURLIndex = 0) {
         this.lastCheckTime = new Date();
         this.checking = true;
         this.emit('checking-for-update');
-        return this._fetchManifest(this.manifestURLs[manifestURLIndex])
-            .then(manifest => {
-                this.checking = false;
-                if (manifest && manifest.isNewerVersionThan(this.currentVersion)) {
-                    console.log(`Updater: new version ${manifest.version}`);
-                    this.newVersion = manifest;
-                    this.emit('update-available', this.newVersion);
-                    this._download(); // start download automatically
-                } else {
-                    this.emit('update-not-available');
+        try {
+            if (await this._check()) {
+                this.emit('update-available', this.newVersion);
+                if (!this.downloading && !this.downloadedFile) {
+                    // Start download automatically.
+                    // Don't care to await it, since it's event-based.
+                    this._downloadUpdate();
                 }
-                return manifest;
-            })
-            .catch(err => {
-                if (manifestURLIndex < this.manifestURLs.length - 1) {
-                    return this.checkForUpdates(manifestURLIndex + 1); // try next URL
-                } else {
-                    this.checking = false;
-                    console.log('Error checking for update: ', err);
-                    this.emit('error', err);
-                }
-            });
+            } else {
+                this.emit('update-not-available');
+            }
+        } catch (err) {
+            this.emit('error', err);
+        }
+    }
+
+    /**
+     * Downloads the update.
+     *
+     * Emits one of:
+     *   'update-downloaded'
+     *   'error'
+     *
+     * @param {string?} [platform] optional platform. Current platform by default.
+     */
+    async _downloadUpdate(platform) {
+        try {
+            await this._download()
+            this.emit('update-downloaded', this.downloadedFile, this.newVersion);
+        } catch (err) {
+            this.emit('error', err);
+        }
     }
 
     /**
@@ -133,6 +151,41 @@ class Updater extends EventEmitter {
     stopCheckingPeriodically() {
         if (this._intervalId) {
             clearInterval(this._intervalId);
+        }
+    }
+
+    /**
+     * Checks for new update.
+     *
+     * Returns a promise resolving to manifest if the new version is found,
+     * to null if it's not found; throws if there was an error.
+     *
+     * Also sets this.newVersion to the new update if it was found.
+     *
+     * @param {number?} manifestURLIndex - optional manifest URL index to check
+     * @returns {Promise<Manifest|null>}
+     */
+   async _check(manifestURLIndex = 0) {
+        this.lastCheckTime = new Date();
+        this.checking = true;
+
+        try {
+            const manifest = await this._fetchManifest(this.manifestURLs[manifestURLIndex]);
+            this.checking = false;
+            if (manifest && manifest.isNewerVersionThan(this.currentVersion)) {
+                console.log(`Updater: new version ${manifest.version}`);
+                this.newVersion = manifest;
+                return manifest;
+            }
+            return null;
+        } catch (err) {
+            if (manifestURLIndex < this.manifestURLs.length - 1) {
+                return await this._check(manifestURLIndex + 1); // try next URL
+            } else {
+                this.checking = false;
+                console.log('Error checking for update: ', err);
+                throw new Error(`Error checking for update: ${err}`);
+            }
         }
     }
 
@@ -196,20 +249,19 @@ class Updater extends EventEmitter {
         });
     }
 
-    /**
-     * Downloads the update.
-     *
-     * @param {string?} [platform] optional platform. Current platform by default.
-     */
-    _download(platform) {
+
+    async _download(platform) {
+        if (this.downloading) {
+            throw new Error('Download is already in progress')
+        }
         if (!this.newVersion) {
-            return Promise.reject(new Error('No new version to download'));
+            throw new Error('No new version to download');
         }
         const address = this.newVersion.getFile(platform);
         const size = this.newVersion.getSize(platform);
         const hash = this.newVersion.getSha512(platform);
         if (address == null || size == null || hash == null) {
-            return Promise.reject(new Error('No file in manifest for the current platform'));
+            throw new Error('No file in manifest for the current platform');
         }
         const tmpfile = path.join(
             this._directory,
@@ -217,30 +269,30 @@ class Updater extends EventEmitter {
         );
         this.downloading = true;
         console.log('Fetching file', address);
-        return this._createDownloadsDirectory()
-            .then(() => fetchFile(address, tmpfile))
-            .then(() => verifySize(size, tmpfile))
-            .then(() => verifyHash(hash, tmpfile))
-            .then(() => {
-                this.downloading = false;
-                this.downloadedFile = tmpfile;
-                this.emit('update-downloaded', this.downloadedFile, this.newVersion);
-                if (this.autoInstall) {
-                    // setup exit hook to install this update
-                    this._setupExitHook();
-                }
-                return tmpfile;
-            })
-            .catch(err => {
-                this.downloading = false;
-                console.log('Error downloading update:', err);
-                this.emit('error', err);
-            });
+
+        try {
+            await this._createDownloadsDirectory();
+            await fetchFile(address, tmpfile);
+            await verifySize(size, tmpfile);
+            await verifyHash(hash, tmpfile);
+            this.downloading = false;
+            this.downloadedFile = tmpfile;
+            if (this.autoInstall) {
+                // setup exit hook to install this update
+                this._setupExitHook();
+            }
+            return tmpfile;
+        } catch (err) {
+            this.downloading = false;
+            console.log('Error downloading update:', err);
+            throw err;
+        }
     }
 
     _setupExitHook() {
-        if (this._exitHookInstalled)
+        if (this._exitHookInstalled) {
             return;
+        }
 
         if (process.versions && process.versions.electron) {
             const { app } = require('electron');
@@ -261,6 +313,8 @@ class Updater extends EventEmitter {
     _createDownloadsDirectory() {
         return new Promise((fulfill, reject) => {
             mkdirp(this._directory, err => {
+                // Note: it's not an error if the directory already exists,
+                // (mkdirp just reports success, which is good.)
                 if (err) {
                     reject(err);
                     return;
@@ -270,8 +324,109 @@ class Updater extends EventEmitter {
         });
     }
 
+    _getUpdateInfoFilePath() {
+        return path.join(this._directory, UPDATE_INFO_FILENAME);
+    }
+
+    _rememberInstallAttempt() {
+        const info = {
+            attempts: 0, // TODO: implement attempt counter
+            currentVersion: this.currentVersion,
+            updateVersion: this.newVersion.version,
+            updateSize: this.newVersion.getSize(),
+            updateHash: this.newVersion.getSha512(),
+            updateFile: this.downloadedFile
+        };
+        return new Promise((fulfill, reject) => {
+            fs.writeFile(this._getUpdateInfoFilePath(), JSON.stringify(info), err => {
+                if (err) {
+                    // Can't write install attempt for some reason.
+                    // Log the error and continue.
+                    console.error('Failed to write install attempt info: ', err);
+                }
+                fulfill();
+            });
+        });
+    }
+
+    _readUpdateInfoFile() {
+        return new Promise((fulfill, reject) => {
+            fs.readFile(this._getUpdateInfoFilePath(), (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                let info;
+                try {
+                    info = JSON.parse(data.toString('utf-8'));
+                } catch (ex) {
+                    reject(ex);
+                    return;
+                }
+                if (!info || !info.currentVersion) {
+                    reject(new Error('Update info is invalid'));
+                    return;
+                }
+                fulfill(info);
+            });
+        });
+    }
+
+    /**
+     * Returns a promise resolving to true if previous update failed.
+     * Should be used after starting a new version.
+     *
+     * @returns {Promise<boolean>}
+     */
+    didLastUpdateFail() {
+        return this._readUpdateInfoFile()
+            .then(info => {
+                // If we're running the same version as one before updating,
+                // the update failed.
+                return (info && (info.currentVersion === this.currentVersion));
+            })
+            .catch(err => {
+                // If there's an error (file doesn't exist, can't be read,
+                // or JSON can't be parsed), we assume update succeeded.
+                return false;
+            });
+    }
+
+    /**
+     * Remove update artifacts.
+     * Should be used after starting a new version.
+     *
+     * Will ignore any filesystem errors, as this is best-effort.
+     */
+    async cleanup() {
+        let info;
+        try {
+            info = await this._readUpdateInfoFile();
+        } catch (err) {
+            // No update info file or reading/parsing it failed,
+            // assume we don't need to cleanup.
+            return;
+        }
+        try {
+            // Delete old downloaded update file.
+            // Make sure the file is inside our downloads
+            // directory, for safety.
+            if (!info.updateFile.startsWith(this._directory)) {
+                return;
+            }
+            await deleteFile(info.updateFile);
+            // Delete update info.
+            await deleteFile(this._getUpdateInfoFilePath());
+        } catch (err) {
+            console.error('Cleanup failed:', err);
+        }
+    }
+
     _install() {
-        if (!this.downloadedFile) return;
+        if (!this.downloadedFile) {
+            console.warn('No update to install');
+            return;
+        }
         console.log('Updater is installing update');
         let install;
         switch (process.platform) {
@@ -294,14 +449,18 @@ class Updater extends EventEmitter {
         install(this.downloadedFile, !!this.restart);
     }
 
-    quitAndInstall() {
+    /**
+     * Installs the update and restarts the app.
+     */
+    async quitAndInstall() {
         this.restart = true;
-
+        await this._rememberInstallAttempt();
         this._setupExitHook();
         if (process.versions && process.versions.electron) {
             const { app } = require('electron');
             if (process.platform === 'linux') {
-                // On Linux, we need to schedule restart here due to AppImage quirks.
+                // On Linux, we need to schedule restart here,
+                // not in the hook, due to AppImage quirks.
                 app.relaunch({
                     args: process.argv.slice(1),
                     execPath: process.env.APPIMAGE
@@ -312,6 +471,70 @@ class Updater extends EventEmitter {
             process.exit(0);
         }
     }
+
+    /**
+     * Retries installing the downloaded update, or
+     * redownloads the update, and restarts the app.
+     */
+    async quitAndRetryInstall(allowLocal = true) {
+        try {
+            this.downloadedFile = await this._getValidUpdateFileOnDisk();
+            this.quitAndInstall();
+        } catch (err) {
+            // Try checking for and downloading the update.
+            try {
+                if (!await this._check()) {
+                    // No update is found... strange.
+                    // Restart anyway by throwing exception.
+                    throw new Error('No update found');
+                }
+                await this._download();
+                // Success, try installing the update.
+                this.quitAndInstall();
+            } catch (ex) {
+                // TODO: record another failed attempt.
+                await this._rememberInstallAttempt();
+                if (process.versions && process.versions.electron) {
+                    const { app } = require('electron');
+                    app.relaunch(); // TODO: check if AppImage needs different execPath
+                    app.quit();
+                } else {
+                    process.exit(0); // won't relaunch if not in Electron (for tests)
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a promise resolving to the path of update file
+     * on disk if and only if it is valid according to info
+     * stored in the update info file; otherwise, throws.
+     */
+    async _getValidUpdateFileOnDisk() {
+        let info = await this._readUpdateInfoFile();
+        if (!info || info.updateSize || !info.updateHash || !info.updateFile) {
+            throw new Error('Invalid update info');
+        }
+        await verifySize(info.updateSize, info.updateFile);
+        await verifyHash(info.updateHash, info.updateFile);
+        // Make sure update file is in our downloads directory.
+        if (!info.updateFile.startsWith(this._directory)) {
+            throw new Error(`Invalid update file path: ${info.updateFile}`);
+        }
+        return info.updateFile;
+    }
+}
+
+function deleteFile(filename) {
+    return Promise.resolve((fulfill, reject) => {
+        fs.unlink(filename, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            fulfill();
+        })
+    });
 }
 
 module.exports = Updater;
